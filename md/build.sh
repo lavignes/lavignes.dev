94b99a914a67e4721e0d336b7969f3035c23a9e2
Scott LaVigne <lavignes@amazon.com>
2023-07-29 23:16:21 -0700

    automatic titles

--- md/build.sh
+++ md/build.sh
@@ -5,15 +5,16 @@ set -e
 MD="comrak -e strikethrough,table,superscript,footnotes --unsafe --syntax-highlighting none"
 DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
 
-echo "dir: $DIR"
-
 find "$DIR" -name "*.md" -not -name "_*.md" | while read -r fname; do
   fdir="$(dirname "$fname")"
   odir="$DIR/..${fdir#"$DIR"}"
   ofname="$odir/$(basename "$fname" md)html"
   printf "transforming %s => %s ... " "$fname" "$ofname"
   mkdir -p "$odir"
+  title=$(head -n 1 "$fname" | sed -e "s/^\\W*#*\\W*//")
   eval "$MD $DIR/_head.md" > "$ofname"
+  echo "<title>$title</title>" >> "$ofname"
+  eval "$MD $DIR/_prebody.md" >> "$ofname"
   eval "$MD $fname" >> "$ofname"
   eval "$MD $DIR/_tail.md" >> "$ofname"
   echo "ok"
